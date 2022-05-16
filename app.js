3af193e9624bc96c6e7e0785ebc59d1494805395
Admin <nguyenhongngoc301201@gmail.com>
2022-05-16 20:41:48 +0700

    first

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+const users = require("./routes/Users");
+
+const port = process.env.PORT || 3000;
+const URI = process.env.MONGODB_URL;
+
+mongoose
+    .connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(port, () => console.log(`http://localhost:${port}`));
+    })
+    .catch((e) => console.log("Unable to connect " + e.message));
---
+++ models/users.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    phoneNumber: {
+        type: String,
+        unique: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    fullName: String,
+    dateOfbirth: Date,
+    address: String,
+    imgName: [{
+        type: String,
+    }, ],
+    username: String,
+    password: String,
+});
+
+module.exports = mongoose.model("users", UserSchema);
