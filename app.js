6669d060810ff199420d6d299333f3ac471cbd13
Hồng Ngọc <94673746+itsmehn@users.noreply.github.com>
2022-05-21 22:33:51 +0700

    update view engine

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const path = require('path')
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
@@ -13,7 +15,7 @@ app.use(
 );
 
 const users = require("./routes/Users");
-
+app.get('/', (req, res) => { res.render('login') })
 const port = process.env.PORT || 3000;
 const URI = process.env.MONGODB_URL;
 
