9e4eb9e78aaefce41a88db7fa395408c6a450ced
Hồng Ngọc <94673746+itsmehn@users.noreply.github.com>
2022-05-30 23:42:59 +0700

    update is Admin

--- middlewares/checkAdmin.js
+++
@@ -1,8 +0,0 @@
-const checkAdmin = (req, res, next) => {
-
-    if (req.session.account && req.session.account.role == 'admin') {
-        return res.redirect('/admin/manageraccount')
-    }
-    next()
-}
-module.exports = checkAdmin
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Router = express.Router()
-const checkAdmin = require('../middlewares/checkAdmin')
+const isAdmin = require('../middlewares/isAdmin')
 
 const adminController = require('../controllers/admin.controller')
 
-Router.get('/manageraccount', adminController.getManagerAccount)
+Router.get('/manageraccount', isAdmin, adminController.getManagerAccount)
     // Router.post('/manageraccount', adminController.postManagerAccount)
     // Router.get('/account', adminController.getAccount)
-Router.get('/account/:id', adminController.getAccount)
+Router.get('/account/:id', isAdmin, adminController.getAccount)
 
 module.exports = Router
